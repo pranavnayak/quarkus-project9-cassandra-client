627c95f3a82784be96d88d568814ae0a90a75c66
Pranav Nayak <pranav18axis@gmail.com>
2021-12-29 12:00:07 +0530

    Update FruitResource.java

--- src/main/java/org/popins/dev/FruitResource.java
+++ src/main/java/org/popins/dev/FruitResource.java
@@ -1,18 +1,4 @@
-/*
- * Copyright DataStax, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.popins.dev;
 
 import java.util.List;
@@ -50,12 +36,5 @@ public class FruitResource {
   public void add(Fruit fruit) {
     fruitService.save(fruit);
   }
-  /*
-  private FruitDto convertToDto(Fruit fruit) {
-    return new FruitDto(fruit.getName(), fruit.getDescription());
-  }
 
-  private Fruit convertFromDto(FruitDto fruitDto) {
-    return new Fruit(fruitDto.getName(), fruitDto.getDescription());
-  }*/
 }
